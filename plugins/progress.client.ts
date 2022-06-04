326c180df065555c0220d6e77e3bcb50343c50e0
mattfletcher94 <mattfletcher94@outlook.com>
2022-06-04 10:04:24 +0100

    bug: unreg sw script twice

--- plugins/progress.client.ts
+++ plugins/progress.client.ts
@@ -1,24 +1,6 @@
 import NProgress from 'nprogress'
 
 export default defineNuxtPlugin(async () => {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    if (!registrations.length) {
-      console.log('No serviceWorker registrations found.')
-      return
-    }
-    for (const registration of registrations) {
-      registration.unregister().then((boolean) => {
-        console.log(
-          (boolean ? 'Successfully unregistered' : 'Failed to unregister'), `ServiceWorkerRegistration\n${
-          registration.installing ? `  .installing.scriptURL = ${registration.installing.scriptURL}\n` : ''
-          }${registration.waiting ? `  .waiting.scriptURL = ${registration.waiting.scriptURL}\n` : ''
-          }${registration.active ? `  .active.scriptURL = ${registration.active.scriptURL}\n` : ''
-          }  .scope: ${registration.scope}\n`,
-        )
-      })
-    }
-  })
-
   NProgress.configure({
     showSpinner: false,
     easing: 'ease',
