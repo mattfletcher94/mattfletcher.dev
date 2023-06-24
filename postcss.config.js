05ae76136d8b306166446954c1a57d068a0b2e57
mattfletcher94 <mattfletcher94@outlook.com>
2023-06-24 08:47:43 +0100

    cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,6 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    cssnano: process.env.NODE_ENV === 'production' ? { preset: ['default', { discardComments: { removeAll: true } }] } : false, // disable cssnano when not in production
   },
 }
